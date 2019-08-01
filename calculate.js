820721bf3b833167b6c4887e2f5e48aceecd025b
vanvinh1234 <vanvinhbk90hoangkim@gmail.com>
2019-08-01 23:57:51 +0700

    Calculater addition subtraction multiplication division

---
+++ calculate.js
@@ -0,0 +1,23 @@
+
+function addition(){
+    let number1 = parseInt(document.getElementById("number1").value);
+    let number2 = parseInt(document.getElementById("number2").value);
+a = document.getElementById("hienthi").innerHTML = number1 + number2
+}
+function subtraction() {
+    let number1 = parseInt(document.getElementById("number1").value);
+    let number2 = parseInt(document.getElementById("number2").value);
+    document.getElementById("hienthi").innerHTML = b = number1 - number2
+}
+function multiplication() {
+    let number1 = parseInt(document.getElementById("number1").value);
+    let number2 = parseInt(document.getElementById("number2").value);
+    document.getElementById("hienthi").innerHTML=c=number1*number2;
+}
+function division(){
+    let number1 = parseInt(document.getElementById("number1").value);
+    let number2 = parseInt(document.getElementById("number2").value);
+    let d=number1/number2;
+    document.getElementById("hienthi").innerHTML=d;
+
+}
